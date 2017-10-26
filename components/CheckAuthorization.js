d45c7162b40492ea22d422cda743d011e9585864
Henri Bergius <henri.bergius@iki.fi>
2017-10-26 17:03:08 +0200

    Implement JWT checking, refs flowhub/bigiot-js#5

--- components/CheckAuthorization.js
+++ components/CheckAuthorization.js
@@ -1,4 +1,5 @@
 const noflo = require('noflo');
+const { provider: BigIotProvider } = require('bigiot-js');
 
 exports.getComponent = function () {
   const c = new noflo.Component();
@@ -15,21 +16,55 @@ exports.getComponent = function () {
     control: true,
     scoped: false,
   });
+  c.inPorts.add('anonymous', {
+    datatype: 'boolean',
+    description: 'Whether to allow anonymous requests',
+    control: true,
+    scoped: false,
+    default: false,
+  });
   c.outPorts.add('req', {
     datatype: 'object',
     description: 'Validated HTTP request',
     scoped: true,
   });
   c.process((input, output) => {
-    if (!input.hasData('req')) {
+    if (!input.hasData('req', 'secret')) {
+      return;
+    }
+    if (input.attached('anonymous').length && !input.hasData('anonymous')) {
       return;
     }
     const req = input.getData('req');
-    // TODO: Actually check token
-    console.log(req.req.headers.Authorization);
-    output.sendDone({
-      req,
-    });
+    const secret = input.getData('secret');
+    let anonymous = false;
+    if (input.hasData('anonymous')) {
+      anonymous = input.getData('anonymous');
+    }
+    if (anonymous && !req.req.headers.authorization) {
+      // Debug mode: allowing anon requests
+      output.sendDone({
+        req,
+      });
+      return;
+    }
+    if (!req.req.headers.authorization) {
+      req.res.status(401).send('Authentication bearer token required');
+      output.done();
+      return;
+    }
+    const prov = new BigIotProvider('auth', secret);
+    const token = req.req.headers.authorization.slice(7);
+    prov.validateToken(token)
+      .then(() => {
+        output.sendDone({
+          req,
+        });
+      })
+      .catch((err) => {
+        req.res.status(403).send(err.message);
+        output.done();
+      });
   });
   return c;
 };
