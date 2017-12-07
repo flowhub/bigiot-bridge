7772249a1a3c9e52d1f1f1b3b195ef5f38178678
Henri Bergius <henri.bergius@iki.fi>
2017-12-07 16:11:29 +0100

    Component for fetching Cologne parking data

---
+++ components/CologneParkingRequest.js
@@ -0,0 +1,34 @@
+const noflo = require('noflo');
+
+exports.getComponent = function () {
+  const c = new noflo.Component();
+  c.description = 'Formulate an API request';
+  c.icon = 'forward';
+  c.inPorts.add('in', {
+    datatype: 'bang',
+    description: 'Prepare query',
+  });
+  c.outPorts.add('out', {
+    datatype: 'object',
+  });
+  c.outPorts.add('error', {
+    datatype: 'object',
+  });
+
+  c.process((input, output) => {
+    if (!input.hasData('in')) {
+      return;
+    }
+    input.getData('in');
+
+    const r = {
+      protocol: 'http:',
+      host: 'www.stadt-koeln.de',
+      path: '/externe-dienste/open-data/parking.php',
+    };
+
+    output.sendDone({ out: r });
+  });
+
+  return c;
+};
